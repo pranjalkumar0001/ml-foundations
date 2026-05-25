bb35a5b6b290d2cac19ea39853e031de7d2dfaef
pranjalkumar0001 <pranjalkumar.pk.0001@gmail.com>
2026-05-25 21:19:03 +0530

    few updates: added substraction, division and checked few edge cases

--- week2/micrograd_engine.py
+++ week2/micrograd_engine.py
@@ -11,6 +11,7 @@ class value:
         return f"value(data={self.data:.4f}, grad={self.grad:.4f})"
     
     def __add__(self, other):
+        other = other if isinstance(other, value) else value(other)
         out = value(self.data + other.data, (self, other), '+')
         def _backward():
             self.grad += 1*out.grad
@@ -18,7 +19,17 @@ class value:
         out._backward = _backward
         return out
     
+    def __radd__(self, other):
+        return self + other
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return other + (-self)
+    
     def __mul__(self, other):
+        other = other if isinstance(other, value) else value(other)
         out = value(self.data * other.data, (self, other), '*')
         def _backward():
             self.grad += other.data * out.grad
@@ -26,6 +37,15 @@ class value:
         out._backward = _backward
         return out
     
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other): 
+        return self * other**-1
+
+    def __rtruediv__(self, other): 
+        return other * self**-1
+
     def sigmoid(self):
         out = value(1/(1+np.exp(-self.data)), (self,), 'sigmoid')
         def _backward():
