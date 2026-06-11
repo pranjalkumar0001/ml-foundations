256bdf54e48dc8d69be289581fc19273be9f4f5b
pranjalkumar0001 <pranjalkumar.pk.0001@gmail.com>
2026-06-11 19:46:53 +0530

    implement 60Hz notch, 8-30Hz bandpass, and event epoching pipeline

--- MNE-python_projects/01_physionet_ingest.py
+++ MNE-python_projects/01_physionet_ingest.py
@@ -10,7 +10,7 @@ for objects in file_path:
     data = read_raw_edf(objects, preload=True)
     raw_objects.append(data)
 raw = concatenate_raws(raw_objects)
-print(raw.info)
+#print(raw.info)
 #raw.compute_psd().plot() # plot without filter
 # plt.show()
 raw.filter(l_freq=8, h_freq= 30)
@@ -19,5 +19,8 @@ raw.notch_filter(freqs=50)
 #raw.compute_psd().plot()
 #plt.show()
 events, event_id_dict = mne.events_from_annotations(raw)
-print("\n\n\n printing evet_id_dict")
-print(event_id_dict)
+print("printing event_id_dict")
+print(event_id_dict) #T00: rest, T1: left hand, T2: right hand
+custom_mapping = {'rest': 1, 'left_fist': 2, 'right_fist': 3}
+epochs = mne.Epochs(raw,events, event_id=custom_mapping, tmin= -1, tmax= 4, preload=True)
+print(epochs)
