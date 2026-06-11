59b9baa9024645c5b697bc0e70f23c4b5ebbb5a2
pranjalkumar0001 <pranjalkumar.pk.0001@gmail.com>
2026-06-11 19:32:06 +0530

    made graphs for unfiltered and filtered eeg signals

--- MNE-python_projects/01_physionet_ingest.py
+++ MNE-python_projects/01_physionet_ingest.py
@@ -1,6 +1,7 @@
 import mne
 from mne.io import read_raw_edf
 from mne.io import concatenate_raws
+import matplotlib.pyplot as plt
 Subject = 1
 run = [4, 8, 12]
 file_path = mne.datasets.eegbci.load_data(Subject, run)
@@ -10,3 +11,13 @@ for objects in file_path:
     raw_objects.append(data)
 raw = concatenate_raws(raw_objects)
 print(raw.info)
+#raw.compute_psd().plot() # plot without filter
+# plt.show()
+raw.filter(l_freq=8, h_freq= 30)
+#raw.compute_psd().plot()
+raw.notch_filter(freqs=50)
+#raw.compute_psd().plot()
+#plt.show()
+events, event_id_dict = mne.events_from_annotations(raw)
+print("\n\n\n printing evet_id_dict")
+print(event_id_dict)
